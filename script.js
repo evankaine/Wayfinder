315b4c1de305bdb85c50a362b37c4b926c000147
Evan Alves <evanalves@Evans-MacBook-Air.local>
2021-06-23 16:36:21 -0400

    basic css and added icons

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ let page = 1
 minusButton.addEventListener('click', event => {
   event.preventDefault();
   removeThis()
-  page--
-
+  if (page > 1) {
+    page--
+  }
     getData(page)
     console.log(page)
     return page
@@ -21,29 +22,33 @@ minusButton.addEventListener('click', event => {
   plusButton.addEventListener('click', event => {
     event.preventDefault();
     removeThis()
-    page++
-    getData(page)
+      if (page < 6) {
+        page++
+      }
+      getData(page)
     console.log(page)
+
     return page
   });
 
-async function getData(a) {
+
+async function getData() {
   try {
     const response = await axios.get(`https://swapi.dev/api/planets/?page=${page}`)
 
     const renderList = (response.data.results)
-    console.log(renderList)
+    // console.log(renderList)
     for (i = 0; i < renderList.length; i++) {
 
       const titleDiv = document.createElement('div')
       titleDiv.classList.add('cards')
       planetList.append(titleDiv)
-
+     
       const titleName = document.createElement('h3')
 
       titleName.textContent = `${renderList[i].name}`
       titleDiv.append(titleName)
-
+      
       const terrain = document.createElement('h4')
       terrain.textContent = `Terrain: ${renderList[i].terrain}`
       titleDiv.append(terrain)
@@ -75,6 +80,9 @@ function removeThis() {
     planetList.removeChild(planetList.lastChild)
   }
 }
+
+
+
 
 
 
